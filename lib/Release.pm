8002952aecc1479e85898f7d2fbe709ec3fc6913
Toyohisa Kameyama <kameyama@riken.jp>
2013-10-17 17:39:54 +0900

    Add LGPL and copyright -> Copyright.

--- lib/Release.pm
+++ lib/Release.pm
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program; if not, write to the Free Software 
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.$
-# $RELEASE_TOOL_VERSION: 0.2$
+# $RELEASE_TOOL_VERSION: 0.3$
 # Written by Toyohisa Kameyama (kameyama@riken.jp)
 package Release;
 use Exporter 'import';
--- lib/test.pl
+++ lib/test.pl
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program; if not, write to the Free Software 
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.$
-# $RELEASE_TOOL_VERSION: 0.2$
+# $RELEASE_TOOL_VERSION: 0.3$
 
 use strict;
 use warnings;
