b28b22b765e1714dc056d1a16684147aa3c2b57c
Toyohisa Kameyama <kameyama@riken.jp>
2021-03-25 09:50:42 +0900

    remove mail address.

--- lib/Release.pm
+++ lib/Release.pm
@@ -1,8 +1,22 @@
-# $RIKEN_copyright: Copyright 2015 RIKEN All rights reserved.$
+# $LGPL2:  This library is free software; you can redistribute it and/or 
+#     modify it under the terms of the GNU Lesser General Public 
+#     License as published by the Free Software Foundation; either 
+#     version 2 of the License, or (at your option) any later version. 
+#  
+#     This library is distributed in the hope that it will be useful, 
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+#     Lesser General Public License for more details. 
+#  
+#     You should have received a copy of the GNU Lesser General Public 
+#     License along with this library; if not, write to the Free Software 
+#     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA $
+# $RELEASE_TOOL_VERSION: 0.33$
+# $RIKEN_copyright: Copyright 2015,2020 RIKEN All rights reserved.$
 # Copyright (c) 2001,2000,1999,1998,1997
 #       Real World Computing Partnership
 # Copyright (C) 2003-2011 PC Cluster Consortium
-# Written by Toyohisa Kameyama (kameyama@riken.jp)
+# Written by Toyohisa Kameyama
 package Release;
 use Exporter 'import';
 use Getopt::Long qw(:config pass_through);
